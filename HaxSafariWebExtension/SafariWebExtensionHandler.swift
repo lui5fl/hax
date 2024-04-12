b100f7a20deba5bd9af6ee4b2c00893ebf135814
Luis Fariña <luis@luisfl.me>
2024-04-12 19:22:25 +0200

    Create Safari web extension (#71)
    feat: create Safari web extension
    
    closes #1

---
+++ HaxSafariWebExtension/SafariWebExtensionHandler.swift
@@ -0,0 +1,18 @@
+//
+//  SafariWebExtensionHandler.swift
+//  HaxSafariWebExtension
+//
+//  Created by Luis Fariña on 10/4/24.
+//
+
+import Foundation
+
+final class SafariWebExtensionHandler: NSObject,
+                                       NSExtensionRequestHandling {
+
+    // MARK: Methods
+
+    func beginRequest(with context: NSExtensionContext) {
+        // Do nothing
+    }
+}
